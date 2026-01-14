bb01002e4048729b06195770af76abec35684225
nevarwin <ravencsolis@gmail.com>
2026-01-14 23:50:06 +0800

    Initial Commit

---
+++ SkyWayRoom/SkyWayRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyWayRoomApp.swift
+//  SkyWayRoom
+//
+//  Created by raven on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct SkyWayRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkyWayRoomTests/SkyWayRoomTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkyWayRoomTests.swift
+//  SkyWayRoomTests
+//
+//  Created by raven on 1/14/26.
+//
+
+import Testing
+@testable import SkyWayRoom
+
+struct SkyWayRoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
